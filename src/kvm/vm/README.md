fa19a929ab12a39a660294ac79f8723c3b928c22
howeyhuo <15116765+howeyhuo@user.noreply.gitee.com>
2024-11-22 18:28:51 +0800

    README.md add schema kvm_vm class

--- src/kvm/vm/README.md
+++ src/kvm/vm/README.md
@@ -6,15 +6,30 @@ This utilities will use all the resource and attribute given by json file and cr
 
 ### schema
  ```jsonc
+vm_{vm_name}.json
 {
     "name": "vm name",      // name of the VM in host
-    "cpu": "host",          // type of cpu for the VM 
+    "cpu": "host",          // optional, type of cpu for the VM .
                             // host: follow means same cpu as vm host
                             // other values: x86、x86_64、ARM、MIPS、PowerPC、SPARC、RISC-V
-    "smp": 2,               // number of CPUs assigned to this VM
+                            // set to host to follow host cpu type by default
+    "smp": 2,               // number of CPUs assigned to this VM, [Symmetric Multi-Processing]
     "ramInGB": 2048,        // number GB of memory to be assigned to this VM
-    "osDrive": "",          // path to KVM OS image JSON data file to read image information
-    "drives": [],           // list of id to retrieve image JSON data file to read image information and fill in command
+    "osDrive": "",          // Drive id to be OS Drive, 
+    "drives": [],           // list of Drive Id to be retrieved from Inventory.
     "networks": [],         // list of id to retrieve network config JSON data file for each nic in VM
+}
+
+drive_{driveIdx}.json
+{
+    "id": "driveIdx",       // Drive id for each drive
+    "linkType": "file",     // Which way to find the drive record, list of ways: [file]
+    "recordFilePath": ""    // Drive Record Path
+}
+
+net_{netIdx}.json
+{
+    "id": "networkIdx",     // Id of the net adapter for the vm
+    "bridgeName": "bridge"  // Bridge name to be linked by VM net adapter
 }
  ```
--- src/kvm/vm/kvm_vm.py
+++ src/kvm/vm/kvm_vm.py
@@ -1,6 +1,55 @@
 #!/usr/bin/env python3
 
+import numbers
+
 from shared.utilities import Util
 from shared.logger import Log
 
+from shared.entity import Entity, ParamEntityProvider, Keyword
+
 logger = Log.get_logger("kvm_vm")
+
+class KvmVm(Entity):
+    class Keyword:
+        CPU = "cpu"
+        SMP = "smp"
+        RamInGb = "ramInGB"
+        OsDrive = "osDrive"
+        Drives = "drives"
+        Networks = "networks"
+
+    def __init__(self, entity_data: dict):
+        super().__init__(entity_data)
+        self.CPU = entity_data.get(self.Keyword.CPU, "host")
+        if self.CPU is None or self.CPU == "":
+            raise ValueError(f"Error: field [{self.Keyword.CPU}] cannot by None or '', 
+                             please specify a valid value or remove it to use default value [host]")
+        self.SMP = entity_data.get(self.Keyword.SMP, None)
+        if self.SMP is None or not isinstance(self.SMP, int):
+            raise ValueError(f"Error: field[{self.Keyword.SMP}] cannot be None and type to be int")
+        self.Ram = entity_data.get(self.Keyword.RamInGb, None)
+        if self.RAM is None or not isinstance(self.Ram, numbers.Number):
+            raise ValueError(f"Error: field[{self.Keyword.RamInGb}] cannot be None and type to be number")
+        self.OsDrive = entity_data.get(self.Keyword.OsDrive, None)
+        if self.OsDrive is None or not isinstance(self.OsDrive, str):
+            raise ValueError(f"Error: field[{self.Keyword.OsDrive}] cannot be None and type to be str")
+        self.Drives = entity_data.get(self.Keyword.Drives, [])
+        if self.Drives is None or not isinstance(self.Drives, list):
+            raise ValueError(f"Error: field[{self.Keyword.Drives}] cannot be None, It's type should be list of str")
+        if len(self.Drives) ==0:
+            raise ValueError(f"Error: field[{self.Keyword.Drives}] cannot be empty list, VM need at least 1 drive to run")
+        for driveIdx in self.Drives:
+            if not isinstance(driveIdx, str) or driveIdx.strip() == "":
+                raise ValueError(f"Error: drive[{driveIdx}] @field[{self.Keyword.Drives}] is not string or is empty")
+        if self.OsDrive not in self.Drives:
+            raise ValueError(f"Error: OS drive[{self.OsDrive}] not in field[{self.Keyword.Drives}]")
+        self.Networks = entity_data.get(self.Keyword.Networks, [])
+        if self.Networks is None or not isinstance(self.Networks, list):
+            raise ValueError(f"Error: field[{self.Keyword.Networks}] cannot be None, It's type should be list of str")
+        
+    def to_json(self, data: dict=None) -> dict:
+        json_data = data if data is not None else {}
+        json_data.update({
+            
+        })
+        return super().to_json(json_data)
