8ca8186bfcbdc1d958085086375fc028be8ebc5b
howeyhuo <15116765+howeyhuo@user.noreply.gitee.com>
2025-02-16 20:05:48 +0800

    add script to prepare kvm environment for kvm manage app

---
+++ src/kvm_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# install all related software
+sudo apt-get install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager
+
+# enable local user able to manage kvm without sudo
+# need reboot to take effect
+sudo adduser $USER kvm
+sudo adduser $USER libvirt
